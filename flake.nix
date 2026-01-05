600a022e0efe50e0893845a21a1260b4cff0de6e
erick-nix <priority.possibly096@passinbox.com>
2026-01-04 22:46:49 -0300

    init astro

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "blog erick-nix";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      };
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = [
+          pkgs.nodejs
+        ];
+
+        shellHook = ''
+          echo "Hello world!"
+        '';
+      };
+    };
+}
